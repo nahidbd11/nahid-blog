ebdda7d04ab871a595bacf7299461b4d221ca7f3
nahid <diegonahid@gmail.com>
2021-11-10 00:11:31 +0600

    worked with json-server

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,38 @@
 import React, { useState, useEffect } from "react";
-import blogdata from "../data";
+
 import BlogLists from "./BlogLists";
 const Home = () => {
-  const [blogs, setBlogs] = useState(blogdata);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  const fetchData = () => {
+    fetch("http://localhost:3004/blogs")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  };
   useEffect(() => {
-    console.log("running after initial render");
+    setTimeout(() => {
+      fetchData();
+      setIsPending(false);
+    }, 3000);
+
+    /*****FIXME: dont use setTimeout in real project .here i am using just for checking loading is working or not****** */
   }, []);
-  function deleteBlogHandler(id) {
-    let newItems = blogs.filter((item) => item.id !== id);
-    setBlogs(newItems);
-  }
+  // function deleteBlogHandler(id) {
+  //   let newItems = blogs.filter((item) => item.id !== id);
+  //   setBlogs(newItems);
+  // }
   return (
     <>
       <div className="blog-list">
-        <BlogLists
-          blogs={blogs}
-          title="All Blogs !!!"
-          deleteBlogHandler={deleteBlogHandler}
-        />
-        <BlogLists
-          blogs={blogs.filter((item) => item.author === "Nahid")}
-          title="Nahids Blogs !"
-          deleteBlogHandler={deleteBlogHandler}
-        />
+        {isPending && <div className="text-warning">Loading ......</div>}
+        {blogs && <BlogLists blogs={blogs} title="All Blogs !!!" />}
+        {blogs && (
+          <BlogLists
+            blogs={blogs.filter((item) => item.author === "Nahid")}
+            title="Nahids Blogs !"
+          />
+        )}
       </div>
     </>
   );
